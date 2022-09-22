542ff2c47a750630554d5da32e28b52eee16e1ac
Tatyana <sanches5896@mail.ru>
2022-09-22 21:10:53 +0300

    Сделанное ДЗ Циклы 1 часть

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,72 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Домашнее задание 1
+        System.out.println("Домашнее задание 1");
+// Задание 1
+        System.out.println("Задание 1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        // Задание 2
+        System.out.println("Задание 2");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        // Задание 3
+        System.out.println("Задание 3");
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println(i);
+
+        }
+// Задание 4
+        System.out.println("Задание 4");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+
+        // Домашнее задание 2
+        System.out.println("Домашнее задание 2");
+
+        // Задание 1
+        System.out.println("Задание 1");
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год является високосным ");
+        }
+
+        // Задание 2
+        System.out.println("Задание 2");
+        for (int i = 7; i <= 98; i = i += 7) {
+            System.out.println(i);
+
+        }
+
+        // Задание 3
+        System.out.println("Задание 3");
+        for (int i = 1; i <= 512; i = i *= 2) {
+            System.out.println(i);
+        }
+
+        // Домашнее задание 3
+        System.out.println("Домашнее задание 3");
+
+        // Задание 1
+        System.out.println("Задание 1");
+
+        int deposit = 29000;
+        int sum = 0;
+        for (int i = 1; i <= 12; i++) {
+            sum += deposit;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + sum + " рублей");
+        }
+        // Задание 2
+        System.out.println("Задание 2");
+        double sumWithPercent = 0;
+        double Percent = 0.01;
+        for (int i = 1; i <= 12; i++) {
+            sumWithPercent = (sumWithPercent + deposit) * (1 + Percent);
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + sumWithPercent + " рублей");
+        }
+
     }
 }
